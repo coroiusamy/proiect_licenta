2d8bc05b2e14e1aa8a55c4f24465b5ea1ffba304
samy <samuel.coroiu@student.upt.ro>
2025-11-30 22:37:24 +0200

    user model update

---
+++ backend/prisma/migrations/20251125174359_add_user_details/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "firstName" TEXT,
+ADD COLUMN     "height" DOUBLE PRECISION,
+ADD COLUMN     "lastName" TEXT,
+ADD COLUMN     "weight" DOUBLE PRECISION;
